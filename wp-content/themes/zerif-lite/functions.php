97651071aca1d33d51940594761525a958b6a6bd
soberanes <paulsoberanes@gmail.com>
2015-05-29 18:45:00 -0500

    Creating plugin

--- wp-content/themes/zerif-lite/functions.php
+++ wp-content/themes/zerif-lite/functions.php
@@ -43,7 +43,7 @@ function zerif_setup()
 
      */
 
-    load_theme_textdomain('zerif-lite', get_template_directory() . '/languages'); 
+    load_theme_textdomain('zerif-lite', get_template_directory() . '/languages');
 
 
     // Add default posts and comments RSS feed links to head.
@@ -144,7 +144,7 @@ function zerif_setup()
         add_image_size('zerif_project_photo', 285, 214, true);
 
         add_image_size('zerif_our_team_photo', 174, 174, true);
-	
+
 		/* woocommerce support */
 		add_theme_support( 'woocommerce' );
 
@@ -248,7 +248,7 @@ add_action('widgets_init', 'zerif_widgets_init');
 
 function zerif_slug_fonts_url() {
     $fonts_url = '';
- 
+
     /* Translators: If there are characters in your language that are not
     * supported by Lora, translate this to 'off'. Do not translate
     * into your own language.
@@ -260,32 +260,40 @@ function zerif_slug_fonts_url() {
     * into your own language.
     */
     $monserrat = _x( 'on', 'Monserrat font: on or off', 'zerif-lite' );
- 
+
     if ( 'off' !== $lato || 'off' !== $monserrat|| 'off' !== $homemade ) {
         $font_families = array();
- 
-        
+
+
         if ( 'off' !== $lato ) {
             $font_families[] = 'Lato:300,400,700,400italic';
         }
- 
+
         if ( 'off' !== $monserrat ) {
             $font_families[] = 'Montserrat:700';
         }
-        
+
         if ( 'off' !== $homemade ) {
             $font_families[] = 'Homemade Apple';
         }
- 
+
         $query_args = array(
             'family' => urlencode( implode( '|', $font_families ) ),
             'subset' => urlencode( 'latin,latin-ext' ),
         );
- 
+
         $fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
     }
- 
+
     return $fonts_url;
+}
+
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+    if (!current_user_can('administrator') && !is_admin()) {
+        show_admin_bar(false);
+    }
 }
 
 /**
@@ -318,9 +326,9 @@ function zerif_scripts()
     wp_enqueue_style('zerif_responsive_style', get_template_directory_uri() . '/css/responsive.css', array('zerif_style'), 'v1');
 
     if ( wp_is_mobile() ){
-        
+
         wp_enqueue_style( 'zerif_style_mobile', get_template_directory_uri() . '/css/style-mobile.css', array('zerif_bootstrap_style', 'zerif_style'),'v1' );
-    
+
     }
 
     wp_enqueue_script('jquery');
@@ -353,7 +361,7 @@ function zerif_scripts()
     /* Vegas script */
 
     wp_enqueue_script('zerif_vegas_script', get_template_directory_uri() . '/js/jquery.vegas.min.js', array("jquery"), '20120206', true);
-	
+
 	/* scrollReveal script */
 	if ( !wp_is_mobile() ){
 		wp_enqueue_script( 'zerif_scrollReveal_script', get_template_directory_uri() . '/js/scrollReveal.js', array("jquery"), '20120206', true  );
@@ -391,7 +399,7 @@ function zerif_register_required_plugins()
 {
 
 	$wp_version_nr = get_bloginfo('version');
-	
+
 	if( $wp_version_nr < 3.9 ):
 
 		$plugins = array(
@@ -401,64 +409,64 @@ function zerif_register_required_plugins()
 
 				'name' => 'Widget customizer',
 
-				'slug' => 'widget-customizer', 
+				'slug' => 'widget-customizer',
 
-				'required' => false 
+				'required' => false
 
 			),
 
 			array(
-	 
+
 				'name'      => 'Login customizer',
-	 
+
 				'slug'      => 'login-customizer',
-	 
+
 				'required'  => false,
-	 
+
 			),
 
 			array(
-	 
+
 				'name'      => 'Revive Old Post (Former Tweet Old Post)',
-	 
+
 				'slug'      => 'tweet-old-post',
-	 
+
 				'required'  => false,
-	 
+
 			)
 
 		);
-		
+
 	else:
 
 		$plugins = array(
 
 			array(
-	 
+
 				'name'      => 'Login customizer',
-	 
+
 				'slug'      => 'login-customizer',
-	 
+
 				'required'  => false,
-	 
+
 			),
 
 			array(
-	 
+
 				'name'      => 'Revive Old Post (Former Tweet Old Post)',
-	 
+
 				'slug'      => 'tweet-old-post',
-	 
+
 				'required'  => false,
-	 
+
 			)
 
 		);
 
-	
+
 	endif;
 
-	 
+
 
 
     $config = array(
@@ -793,15 +801,15 @@ class zerif_ourfocus extends WP_Widget
 
 			<?php if( !empty($instance['image_uri']) ): ?>
             <div class="service-icon">
-				
+
 				<?php if( !empty($instance['link']) ): ?>
-				
+
 					<a href="<?php echo $instance['link']; ?>"><i class="pixeden" style="background:url(<?php echo esc_url($instance['image_uri']); ?>) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON--></a>
-				
+
 				<?php else: ?>
-				
+
 					<i class="pixeden" style="background:url(<?php echo esc_url($instance['image_uri']); ?>) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
-				
+
 				<?php endif; ?>
 
 
@@ -812,14 +820,14 @@ class zerif_ourfocus extends WP_Widget
             <!-- FOCUS HEADING -->
 
 
-			<?php 
+			<?php
 				if( !empty($instance['text']) ):
-				
+
 					echo '<p>';
 						echo apply_filters('widget_title', $instance['text']);
 					echo '</p>';
 				endif;
-			?>	
+			?>
 
         </div>
 
@@ -841,7 +849,7 @@ class zerif_ourfocus extends WP_Widget
         $instance['text'] = strip_tags($new_instance['text']);
 
         $instance['title'] = strip_tags($new_instance['title']);
-		
+
 		$instance['link'] = strip_tags( $new_instance['link'] );
 
         $instance['image_uri'] = strip_tags($new_instance['image_uri']);
@@ -877,8 +885,8 @@ class zerif_ourfocus extends WP_Widget
                    class="widefat"/>
 
         </p>
-		
-	
+
+
 		<p>
 
 			<label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Link','zerif'); ?></label><br />
@@ -995,7 +1003,7 @@ class zerif_testimonial_widget extends WP_Widget
                 <div class="client-info">
 
 					<a class="client-name" target="_blank" <?php if( !empty($instance['link']) ): echo 'href="'.esc_url($instance['link']).'"'; endif; ?>><?php if( !empty($instance['title']) ): echo apply_filters('widget_title', $instance['title'] ); endif; ?></a>
-					
+
 
 					<?php if( !empty($instance['details']) ): ?>
                     <div class="client-company">
@@ -1008,7 +1016,7 @@ class zerif_testimonial_widget extends WP_Widget
                 </div>
 
                 <?php
-				
+
 				if( !empty($instance['image_uri']) ):
 
 					echo '<div class="client-image hidden-xs">';
@@ -1016,7 +1024,7 @@ class zerif_testimonial_widget extends WP_Widget
 					echo '<img src="' . esc_url($instance['image_uri']) . '" alt="">';
 
 					echo '</div>';
-				endif;	
+				endif;
 
                 ?>
 
@@ -1049,7 +1057,7 @@ class zerif_testimonial_widget extends WP_Widget
         $instance['details'] = strip_tags($new_instance['details']);
 
         $instance['image_uri'] = strip_tags($new_instance['image_uri']);
-		
+
 		$instance['link'] = strip_tags( $new_instance['link'] );
 
         return $instance;
@@ -1073,7 +1081,7 @@ class zerif_testimonial_widget extends WP_Widget
                    class="widefat"/>
 
         </p>
-		
+
 		<p>
 
 			<label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Author link','zerif'); ?></label><br />
@@ -1322,7 +1330,7 @@ class zerif_team_widget extends WP_Widget
             <div class="team-member">
 
 				<?php if( !empty($instance['image_uri']) ): ?>
-				
+
 					<figure class="profile-pic">
 
 
@@ -1330,22 +1338,22 @@ class zerif_team_widget extends WP_Widget
 
 
 					</figure>
-				
+
 				<?php endif; ?>
 
 
                 <div class="member-details">
 
 					<?php if( !empty($instance['name']) ): ?>
-					
+
 						<h5 class="dark-text red-border-bottom"><?php echo apply_filters('widget_title', $instance['name']); ?></h5>
-						
-					<?php endif; ?>	
+
+					<?php endif; ?>
 
 					<?php if( !empty($instance['position']) ): ?>
-					
+
 						<div class="position"><?php echo apply_filters('widget_title', $instance['position']); ?></div>
-				
+
 					<?php endif; ?>
 
                 </div>
@@ -1376,7 +1384,7 @@ class zerif_team_widget extends WP_Widget
                             <li><a href="<?php echo apply_filters('widget_title', $instance['db_link']); ?>"><i
                                         class="fa fa-dribbble"></i></a></li>
                         <?php endif; ?>
-						
+
 						<?php if ( !empty($instance['ln_link']) ): ?>
                             <li><a href="<?php echo apply_filters('widget_title', $instance['ln_link']); ?>"><i
                                         class="fa fa-linkedin"></i></a></li>
@@ -1433,7 +1441,7 @@ class zerif_team_widget extends WP_Widget
         $instance['bh_link'] = strip_tags($new_instance['bh_link']);
 
         $instance['db_link'] = strip_tags($new_instance['db_link']);
-		
+
 		$instance['ln_link'] = strip_tags($new_instance['ln_link']);
 
         $instance['image_uri'] = strip_tags($new_instance['image_uri']);
@@ -1825,7 +1833,7 @@ function recaptcha_scripts() {
 add_filter( 'body_class', 'remove_class_function' );
 function remove_class_function( $classes ) {
 
-    if ( !is_home() ) {   
+    if ( !is_home() ) {
         // index of custom-background
         $key = array_search('custom-background', $classes);
         // remove class
@@ -1834,3 +1842,57 @@ function remove_class_function( $classes ) {
     return $classes;
 
 }
+
+// Display Fields
+add_action( 'woocommerce_product_options_general_product_data', 'woo_add_custom_general_fields' );
+function woo_add_custom_general_fields() {
+
+  global $woocommerce, $post;
+
+  echo '<div class="options_group">';
+
+  // Text Field
+  woocommerce_wp_text_input(
+  	array(
+  		'id'          => '_num_pedimento',
+  		'label'       => __( 'Num. de pedimento', 'woocommerce' ),
+  		'placeholder' => 'num. pedimento',
+  		'desc_tip'    => 'true',
+  		'description' => __( 'Ingresa el num. de pedimento.', 'woocommerce' )
+  	)
+  );
+  woocommerce_wp_text_input(
+  	array(
+  		'id'          => '_fecha',
+  		'label'       => __( 'Fecha', 'woocommerce' ),
+  		'placeholder' => 'dd/mm/aaaa',
+  		'desc_tip'    => 'true',
+  		'description' => __( 'Ingresa la fecha.', 'woocommerce' )
+  	)
+  );
+  woocommerce_wp_text_input(
+  	array(
+  		'id'          => '_aduana',
+  		'label'       => __( 'Aduana', 'woocommerce' ),
+  		'placeholder' => 'aduana',
+  		'desc_tip'    => 'true',
+  		'description' => __( 'Ingresa la aduana.', 'woocommerce' )
+  	)
+  );
+  echo '</div>';
+
+}
+
+// Save Fields
+add_action( 'woocommerce_process_product_meta', 'woo_add_custom_general_fields_save' );
+function woo_add_custom_general_fields_save( $post_id ){
+
+	// Text Field
+	$woocommerce_text_field = $_POST['_num_pedimento'];
+	if( !empty( $woocommerce_text_field ) )
+		update_post_meta( $post_id, '_num_pedimento', esc_attr( $woocommerce_text_field ) );
+		update_post_meta( $post_id, '_fecha', esc_attr( $woocommerce_text_field ) );
+		update_post_meta( $post_id, '_aduana', esc_attr( $woocommerce_text_field ) );
+
+
+  }
